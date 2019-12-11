e296a3ea2c8b471d5d36969a7b80cc62a163fc6f
dlevenson44 <daniellevenson4@gmail.com>
2019-12-11 17:59:42 -0500

    Add edit functionality to todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Todo({ todo, remove }) {
+function Todo({ index, todo, remove, update }) {
+  const [isEditing, setEdit] = useState(false);
+  const [text, setText] = useState(todo);
+
+  function updateTask(text, index) {
+    update(text, index);
+    setEdit(!isEditing);
+  };
+
   return (
     <div>
-      <p className="todo-item">{todo}</p>
-      <ion-icon name="trash" onClick={() => remove(todo)} />
-      <ion-icon name="create" onClick={() => console.log('editing task')} />
+      {!isEditing ? (
+        <div>
+          <p className="todo-item">{text}</p>
+          <ion-icon name="create" onClick={() => setEdit(!isEditing)} />
+          <ion-icon name="trash" onClick={() => remove(todo)} />
+        </div>
+      ) : (
+        <div>
+          <input
+            id="create-field"
+            placeholder="Create New Item"
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
+          <ion-icon name="create" onClick={() => updateTask(text, index)} />
+          <ion-icon name="close-circle" onClick={() => setEdit(!isEditing)} />
+        </div>
+      )}
+      
     </div>
   );
 }
